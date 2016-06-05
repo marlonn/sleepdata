7c37685e05625f8eb7c5074be1864872465ffff0
marlonn <marlon_neumann@gmx.de>
2016-06-05 14:26:42 +0200

    period_label 'laying awake' for after aufgewacht

--- app/models/raw_datum.rb
+++ app/models/raw_datum.rb
@@ -11,7 +11,7 @@ class RawDatum < ActiveRecord::Base
     data = []
     id = 0
     can_edit = false
-    RawDatum.find_each do |raw|
+    RawDatum.find_each(start: 140, batch_size: 30) do |raw|
       if raw.status.chomp == "ins Bett gelegt"
         data[id] = ProcessedDatum.create(period_label: "sleep",
                                   begin: Time.parse(raw.timestamp) + 30*60,
@@ -25,8 +25,26 @@ class RawDatum < ActiveRecord::Base
         can_edit = false
       end
     end
+    id += 1
+    can_edit = false
+    RawDatum.find_each(start: 140, batch_size: 30) do |raw|
+      if raw.status.chomp == "aufgewacht"
+        data[id] = ProcessedDatum.create(period_label: "laying awake",
+                                        begin: raw.timestamp)
+        can_edit = true
+      elsif raw.status.chomp == "aufgestanden" and can_edit == true
+        data[id].end = raw.timestamp
+        can_edit = false
+        id += 1
+      # elsif raw.status.chomp != "aufgestanden" and can_edit == true
+      #   data[id] = ProcessedDatum.destroy
+      #   can_edit = false
+      end
+    end
     data
   end
+
+#  def self.process_awake
 
 end
 
