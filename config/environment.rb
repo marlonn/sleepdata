f9edb140701f67916fbd40e1bd26ef74092aa7de
marlonn <marlon_neumann@gmx.de>
2016-06-11 13:23:16 +0200

    installed sass and made .css.sass file with variables for colors

--- config/environment.rb
+++ config/environment.rb
@@ -6,10 +6,12 @@ Sleepdata::Application.initialize!
 
 class String
   def holiday? # on timestamp as string
+
     if Holiday.where(date: Date.parse(self)).any?
       true
     else
       false
     end
+
   end
 end
